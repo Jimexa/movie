47c1cee8ede123c986c47579cd81af2073fb9a6e
Dimitris Exadaktylos <jimexa@yahoo.gr>
2019-11-26 13:04:22 +0200

    adjust movies

--- back-end/src/main/resources/data.sql
+++ back-end/src/main/resources/data.sql
@@ -1,26 +1,26 @@
 DROP TABLE IF EXISTS MOVIES;
 
 INSERT INTO Movie (TITLE, YEAR_ESTABLISHED, USERINSERT, TIMEINSERT) VALUES
-('TO KEFALAIO', '1867-09-25', 'EXADAKTYLOS', '2019-09-25');
+('Star Wars: A new hope', '1977-05-25', 'EXADAKTYLOS', '2019-09-25');
 
 INSERT INTO Movie (TITLE, YEAR_ESTABLISHED, USERINSERT, TIMEINSERT) VALUES
-('KRATOS KAI EPANASTASH', '1917-09-25', 'EXADAKTYLOS', '2019-09-25');
+('Star Wars: The Empire Strikes Back', '1980-09-25', 'EXADAKTYLOS', '2019-09-25');
 
 INSERT INTO Movie (TITLE, YEAR_ESTABLISHED, USERINSERT, TIMEINSERT) VALUES
-('AREIANARA', '1914-09-25', 'EXADAKTYLOS', '2019-09-25');
+('Star Wars: Return of the Jedi', '1983-05-25', 'EXADAKTYLOS', '2019-09-25');
 
 
 
 DROP TABLE IF EXISTS MOVIE_DETAILS;
 
 INSERT INTO Details (DIRECTOR, DESC, USERINSERT, TIMEINSERT, MOVIE_ID) VALUES
-('MARX', 'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto', 'EXADAKTYLOS', '2019-09-25', 1);
+('George Lucas', 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire_s world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.', 'EXADAKTYLOS', '2019-09-25', 1);
 
 INSERT INTO Details (DIRECTOR, DESC, USERINSERT, TIMEINSERT, MOVIE_ID) VALUES
-('LENIN', 'est rerum tempore vitae sequi sint nihil reprehenderit dolor beatae ea dolores neque fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis qui aperiam non debitis possimus qui neque nisi nulla', 'EXADAKTYLOS', '2019-09-25', 2);
+('George Lucas', 'After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda, while his friends are pursued by Darth Vader.', 'EXADAKTYLOS', '2019-09-25', 2);
 
 INSERT INTO Details (DIRECTOR, DESC, USERINSERT, TIMEINSERT, MOVIE_ID) VALUES
-('PATSIMAS', 'et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut', 'EXADAKTYLOS', '2019-09-25', 3);
+('George Lucas', 'After a daring mission to rescue Han Solo from Jabba the Hutt, the Rebels dispatch to Endor to destroy the second Death Star. Meanwhile, Luke struggles to help Darth Vader back from the dark side without falling into the Emperor_s trap.', 'EXADAKTYLOS', '2019-09-25', 3);
 
 
 
